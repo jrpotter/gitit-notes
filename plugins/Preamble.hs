9e0f4d9854f3a202e664681d542dd4c121392899
Joshua Potter <jrpotter2112@gmail.com>
2015-06-21 23:29:37 -0400

    Functional preamble

--- plugins/Preamble.hs
+++ plugins/Preamble.hs
@@ -2,6 +2,7 @@ module Preamble (plugin) where
 
 import Text.Pandoc.Builder
 import Network.Gitit.Interface
+import Data.Char (toLower)
 
 plugin :: Plugin
 plugin = PageTransform prepend
@@ -10,12 +11,12 @@ prepend :: Pandoc -> PluginM Pandoc
 prepend doc@(Pandoc m blks) = do
     meta <- askMeta
     case lookup "preamble" meta of
-      Just s -> return $ Pandoc m (preamble <> blks)
+      Just s | map toLower s == "true" ->
+        return $ Pandoc m (preamble <> blks)
       _      -> return doc
 
 preamble :: [Block]
-preamble = toList $
-    rawBlock "html" "<p style=\"display: none\">" <>
-    plain (fromList [Str "Test", Space, Str "Test"]) <>
-    rawBlock "html" "</p>"
+preamble = toList $ divWith ("", [], [("style", "display:none;")]) $
+    plain (math "\\newcommand{\\bbc}{\\mathbb{C}}") <>
+    plain (math "\\newcommand{\\bbr}{\\mathbb{R}}")
 
